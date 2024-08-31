0f9f36a582b4fa5cf3fea96f425cfa817b290cea
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-09-01 02:38:50 +0545

    fixed

--- components/finetune_model_component.py
+++ components/finetune_model_component.py
@@ -14,6 +14,9 @@ def finetune_model(dataset: Input[Dataset], output_model: Output[Model]):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+    dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+    load_in_4bit = True #
     model, tokenizer = FastLanguageModel.from_pretrained(
         model_name="unsloth/Phi-3-mini-4k-instruct",
         dtype=None,
@@ -21,20 +24,42 @@ def finetune_model(dataset: Input[Dataset], output_model: Output[Model]):
         load_in_4bit=True,
     )
     #model.to(device)
-
-    # Load and prepare dataset
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                        "gate_proj", "up_proj", "down_proj",],
+        lora_alpha = 16,
+        lora_dropout = 0, # Supports any, but = 0 is optimized
+        bias = "none",    # Supports any, but = "none" is optimized
+        # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+        use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+        random_state = 3407,
+        use_rslora = False,  # We support rank stabilized LoRA
+        loftq_config = None, # And LoftQ
+    )
+        # Load and prepare dataset
     dataset = load_dataset('csv', data_files=dataset.path)
     
     # Define training arguments
     trainer = SFTTrainer(
         model=model,
         tokenizer=tokenizer,
-        train_dataset=dataset["train"],
+        train_dataset=dataset,
+        dataset_text_field = "text",
+        max_seq_length = max_seq_length,
+        dataset_num_proc = 2,
+        packing = False,
         args=TrainingArguments(
             per_device_train_batch_size=2,
             gradient_accumulation_steps=4,
+            warmup_steps = 5,
             max_steps=20,
             learning_rate=2e-4,
+            optim = "adamw_8bit",
+            weight_decay = 0.01,
+            lr_scheduler_type = "linear",
+            seed = 3407,
             fp16=not is_bfloat16_supported(),
             bf16=is_bfloat16_supported(),
             logging_steps=1,
--- components/prepare_dataset_component.py
+++ components/prepare_dataset_component.py
@@ -2,12 +2,55 @@ import kfp.dsl as dsl
 from kfp.v2.dsl import component, Output, Dataset
 
 @component(
-    base_image='python:3.8',
+    base_image='us-docker.pkg.dev/brave-smile-424210-m0/train/phi-3:dev',
     packages_to_install=['datasets']
 )
 def prepare_dataset(output_dataset: Output[Dataset]):
+    
+    import os
+    import torch
+    from unsloth import FastLanguageModel
     from datasets import load_dataset
+    from transformers import TrainingArguments
+    from trl import SFTTrainer
+    from unsloth import is_bfloat16_supported
+    
+    max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+    dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+    load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+    # Model and tokenizer setup
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name="unsloth/Phi-3-mini-4k-instruct",
+        dtype = dtype,
+        max_seq_length=2048,
+        load_in_4bit=True,
+    )
+    #data preprocessing prompt
+    alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
-    dataset = load_dataset("yahma/alpaca-cleaned", split="train")
-    # Save the dataset to the specified output path
+    ### Instruction:
+    {}
+
+    ### Input:
+    {}
+
+    ### Response:
+    {}"""
+
+    EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+    def formatting_prompts_func(examples):
+        instructions = examples["instruction"]
+        inputs       = examples["input"]
+        outputs      = examples["output"]
+        texts = []
+        for instruction, input, output in zip(instructions, inputs, outputs):
+            # Must add EOS_TOKEN, otherwise your generation will go on forever!
+            text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+            texts.append(text)
+        return { "text" : texts, }
+    pass
+
+    from datasets import load_dataset
+    dataset = load_dataset("yahma/alpaca-cleaned", split = "train")
+    dataset = dataset.map(formatting_prompts_func, batched = True,)
     dataset.to_csv(output_dataset.path + ".csv")
